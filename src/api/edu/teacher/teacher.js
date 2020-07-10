de376a1e45a5a1e032615932021b99d72e1d1fb9
ZR20060157-刘子安 <2282816304@qq.com>
2020-07-10 17:53:09 +0800

    测试

---
+++ src/api/edu/teacher/teacher.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+export default {
+    //讲师列表
+    getTeacherListPage(current,limit,teacherQuery){
+        return request({
+            // url: '/eduservice/teacher/pageCond',
+            url: `/eduservice/teacher/pageCond/${current}/${limit}`,
+            method: 'get',
+            teacherQuery
+          })
+    }
+}
---
+++ src/views/edu/teacher/add.vue
@@ -0,0 +1,5 @@
+<template>
+    <el-row>
+      <el-col :span="s">add</el-col>
+    </el-row>
+</template>
